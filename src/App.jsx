c4ca96c842f59d15df24f0219fb4c5a910bbf987
Virtual Epoch <cdk11235@gmail.com>
2023-11-09 12:08:42 -0800

    added PerformanceMonitor

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,43 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
-import { Loader, SoftShadows } from "@react-three/drei";
-import { Suspense } from "react";
+import { Loader, PerformanceMonitor, SoftShadows } from "@react-three/drei";
+import { Suspense, useState } from "react";
 import { Physics } from "@react-three/rapier";
 import { Bloom, EffectComposer } from "@react-three/postprocessing";
 import { Leaderboard } from "./components/Leaderboard";
 
 function App() {
+  const [downgradedPerformance, setDowngradedPerformance] = useState(false);
   return (
     <>
       <div className="canvas-overlay">
-        <h1 className="game-title">Light<span className="cross-symbol">⁜</span>Skull</h1>
+        <h1 className="game-title">
+          Light<span className="cross-symbol">⁜</span>Skull
+        </h1>
       </div>
       <Loader />
       <Leaderboard />
       <Canvas shadows camera={{ position: [0, 30, 0], fov: 30, near: 2 }}>
         <color attach="background" args={["#00ffff"]} />
         <SoftShadows size={42} />
+        <PerformanceMonitor
+          // Detect low performance devices
+          onDecline={(fps) => {
+            setDowngradedPerformance(true);
+          }}
+        />
         <Suspense>
           <Physics>
             {/* NOTE: add 'debug' prop above to '<Physics>' to add a wireframe to the rigid bodies */}
             <Experience />
           </Physics>
         </Suspense>
-        <EffectComposer disableNormalPass>
-          <Bloom luminanceThreshold={1} intensity={1.5} mipmapBlur />
-        </EffectComposer>
+        {!downgradedPerformance && (
+          // disable the postprocessing on low-end devices
+          <EffectComposer disableNormalPass>
+            <Bloom luminanceThreshold={1} intensity={1.5} mipmapBlur />
+          </EffectComposer>
+        )}
       </Canvas>
     </>
   );

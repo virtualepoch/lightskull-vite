852ed5ca6329ed96b20502c810058fa577e0a5b7
Virtual Epoch <cdk11235@gmail.com>
2023-11-26 23:54:14 -0800

    css grid mobile testing

--- src/components/ui-components/Menu.jsx
+++ src/components/ui-components/Menu.jsx
@@ -1,23 +1,30 @@
 import { CSSTransition } from "react-transition-group";
 
 export const Menu = ({ menuOpen, gameMap, setGameMap }) => {
+  console.log(window);
   return (
     <CSSTransition in={menuOpen} unmountOnExit timeout={500} classNames="menu">
       <nav className="menu">
-        <h1 className="menu-header">Menu</h1>
-          <h2 className="sub-menu-header">Maps</h2>
+        <header className="menu-header">
+          <h1>Menu:</h1>
+          <h2>Maps</h2>
+        </header>
         <section className="maps">
           <button
             className={gameMap === "map-1" ? "map-btn active" : "map-btn"}
             onClick={() => setGameMap("map-1")}
           >
             Map - 1
+            <br />
+            Tap / Click to Select
           </button>
           <button
             className={gameMap === "map-2" ? "map-btn active" : "map-btn"}
             onClick={() => setGameMap("map-2")}
           >
             Map - 2
+            <br />
+            Tap / Click to Select
           </button>
 
           <button
@@ -25,12 +32,32 @@ export const Menu = ({ menuOpen, gameMap, setGameMap }) => {
             onClick={() => setGameMap("map-3")}
           >
             Map - 3
+            <br />
+            Tap / Click to Select
           </button>
           <button
             className={gameMap === "map-4" ? "map-btn active" : "map-btn"}
             onClick={() => setGameMap("map-4")}
           >
             Map - 4
+            <br />
+            Tap / Click to Select
+          </button>
+          <button
+            className={gameMap === "map-4" ? "map-btn active" : "map-btn"}
+            onClick={() => setGameMap("map-4")}
+          >
+            Map - 4
+            <br />
+            Tap / Click to Select
+          </button>
+          <button
+            className={gameMap === "map-4" ? "map-btn active" : "map-btn"}
+            onClick={() => setGameMap("map-4")}
+          >
+            Map - 4
+            <br />
+            Tap / Click to Select
           </button>
         </section>
       </nav>
